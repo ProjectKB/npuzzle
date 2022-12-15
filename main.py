33b444982ed376a04b9bc533f46a13d499ce2d69
Loïc Bertin <bertin.loic01@gmail.com>
2022-12-15 16:47:05 +0100

    parsing ok

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 import math
 
 
-class Tools:
+class Error:
     # Colors
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -22,6 +22,7 @@ class Tools:
     PERMISSION_ERROR = "PermissionError"
     UNICODE_DECODE_ERROR = "UnicodeDecodeError"
     FILE_FORMAT_ERROR = "FileFormatError"
+    KEYBOARD_INTERRUPT_ERROR = "KeyboardInterruptError"
 
     @staticmethod
     def print_error(msg):
@@ -30,7 +31,7 @@ class Tools:
 
     @staticmethod
     def throw(level, type, msg):
-        print(f"\n\t{level}[{type}] -> {Tools.ENDC}{msg}\n")
+        print(f"\n\t{level}[{type}] -> {Error.ENDC}{msg}\n")
         sys.exit(0)
 
 
@@ -130,13 +131,13 @@ class Parser:
             f = open(self.args.file, 'r')
             file = f.readlines()
         except FileNotFoundError:
-            Tools.throw(Tools.FAIL, Tools.FILE_NOT_FOUND_ERROR, f"No such file: {self.args.file}")
+            Error.throw(Error.FAIL, Error.FILE_NOT_FOUND_ERROR, f"No such file: {self.args.file}")
         except IsADirectoryError:
-            Tools.throw(Tools.FAIL, Tools.IS_A_DIRECTORY_ERROR, f"Is a directory: {self.args.file}")
+            Error.throw(Error.FAIL, Error.IS_A_DIRECTORY_ERROR, f"Is a directory: {self.args.file}")
         except PermissionError:
-            Tools.throw(Tools.FAIL, Tools.PERMISSION_ERROR, f"Permission denied: {self.args.file}")
+            Error.throw(Error.FAIL, Error.PERMISSION_ERROR, f"Permission denied: {self.args.file}")
         except UnicodeDecodeError:
-            Tools.throw(Tools.FAIL, Tools.UNICODE_DECODE_ERROR, f"'utf-8' codec can't decode byte: {self.args.file}")
+            Error.throw(Error.FAIL, Error.UNICODE_DECODE_ERROR, f"'utf-8' codec can't decode byte: {self.args.file}")
 
         for line in file:
             # remove comment
@@ -150,26 +151,36 @@ class Parser:
             line = line.replace('\n', '')
 
             # search for size
-            r = re.search(Parser.SIZE_PATTERN, line)
-            if r:
-                size = int(line)
-                if size == 1:
-                    Tools.throw(Tools.FAIL, Tools.FILE_FORMAT_ERROR, f"File format error: minimum size is 2")
-                continue
+            if not size:
+                r = re.search(Parser.SIZE_PATTERN, line)
+                if r:
+                    size = int(line)
+                    if size < 2:
+                        Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: minimum size is 2, found {size}")
+                    continue
 
             # search for forbidden chars
             r = re.search(Parser.OTHER_THAN_SPACE_DIGITS_PATTERN, line)
             if r:
-                Tools.throw(Tools.FAIL, Tools.FILE_FORMAT_ERROR, f"File format error: forbidden char: {line}")
+                Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: forbidden char: {line}")
 
             # search for missing size
             if not size:
-                Tools.throw(Tools.FAIL, Tools.FILE_FORMAT_ERROR, "File format error: size of puzzle is missing")
+                Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, "File format error: size of puzzle is missing")
+
+            # check that there are at least size numbers on the line
+            if len(line.split()) is not size:
+                Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: {size} numbers are expected for "
+                                                                 f"one line: {line}")
 
             # save line
             puzzle.append(line.split())
 
         f.close()
+
+        # search for missing size
+        if not size:
+            Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, "File format error: size of puzzle is missing")
 
         # generate puzzle control template
         puzzle_control = {str(i): False for i in range(size ** 2)}
@@ -178,20 +189,96 @@ class Parser:
         for row in puzzle:
             for nb in row:
                 if puzzle_control.get(nb) is None:
-                    Tools.throw(Tools.FAIL, Tools.FILE_FORMAT_ERROR, f"File format error: number too big: {nb}")
+                    Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: number too big: {nb}")
                 elif puzzle_control.get(nb) is not False:
-                    Tools.throw(Tools.FAIL, Tools.FILE_FORMAT_ERROR, f"File format error: number repeated: {nb}")
+                    Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: number repeated: {nb}")
                 puzzle_control[nb] = nb
 
         # check that no number is missing
         if False in puzzle_control.values():
-            Tools.throw(Tools.FAIL, Tools.FILE_FORMAT_ERROR,
+            Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR,
                         f"File format error: at least one of 0..{size ** 2 - 1} number is missing")
 
         return Puzzle(size, puzzle)
 
+    def get_input(self):
+        contents = []
+        while True:
+            try:
+                line = input()
+            except EOFError:
+                break
+            except UnicodeDecodeError:
+                Error.throw(Error.FAIL, Error.UNICODE_DECODE_ERROR,
+                            f"'utf-8' codec can't decode byte: {self.args.file}")
+            except KeyboardInterrupt:
+                Error.throw(Error.FAIL, Error.KEYBOARD_INTERRUPT_ERROR, "Use Ctrl+D to interrupt input reading")
+
+            contents.append(line)
+        return contents
+
     def parse_input(self):
-        print(self.args.input)
+        input = self.get_input()
+        size = False
+        puzzle = []
+
+        for line in input:
+            # remove comment
+            line = re.sub(Parser.COMMENT_PATTERN, '', line)
+
+            # skip new/empty line
+            if line == "\n" or line == "":
+                continue
+
+            # remove space
+            line = line.strip()
+
+            # search for size
+            if not size:
+                r = re.search(Parser.SIZE_PATTERN, line)
+                if not r:
+                    Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: forbidden char: {line}")
+                size = int(line)
+                if size < 2:
+                    Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: minimum size is 2, found {size}")
+                continue
+
+            # search for forbidden chars
+            r = re.search(Parser.SIZE_PATTERN, line)
+            if not r:
+                Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: forbidden char: {line}")
+
+            puzzle.append(line)
+
+        # search for missing size
+        if not size:
+            Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, "File format error: size of puzzle is missing")
+
+        # check puzzle len then transform is format
+        len_puzzle = len(puzzle)
+        if len_puzzle is not size ** 2:
+            Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR,
+                        f"File format error: size is {size}, {size ** 2} numbers should have been provided, found {len_puzzle}")
+        puzzle = [[puzzle[j + size * i] for j in range(size)] for i in range(size)]
+
+        # generate puzzle control template
+        puzzle_control = {str(i): False for i in range(size ** 2)}
+
+        # check that no number is repeated or too big
+        for row in puzzle:
+            for nb in row:
+                if puzzle_control.get(nb) is None:
+                    Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: number too big: {nb}")
+                elif puzzle_control.get(nb) is not False:
+                    Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR, f"File format error: number repeated: {nb}")
+                puzzle_control[nb] = nb
+
+        # check that no number is missing
+        if False in puzzle_control.values():
+            Error.throw(Error.FAIL, Error.FILE_FORMAT_ERROR,
+                        f"File format error: at least one of 0..{size ** 2 - 1} number is missing")
+
+        return Puzzle(size, puzzle)
 
     def determine_format(self):
         return 'file' if self.args.file else 'input'
@@ -212,7 +299,7 @@ if __name__ == '__main__':
 
     parser.add_argument("-f", "--file", default=False,
                         help="File containing the puzzle. Overrides -i")
-    parser.add_argument("-i", "--input", default=False,
+    parser.add_argument("-i", "--input", action="store_true", default=False,
                         help="Read the puzzle as string")
 
     args = parser.parse_args()
