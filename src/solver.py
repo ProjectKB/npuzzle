07c17e709a4791ccf6227897e608f243745df317
Simon CROS <simon.dc.cros@gmail.com>
2022-12-16 20:59:50 +0100

    Fixed compilation

--- src/solver.py
+++ src/solver.py
@@ -16,33 +16,30 @@ class Solver:
         # A* but h == 0 for every node resulting in BFS algorithm (nodes are explored in width, and EVERY one of them are visited)
         pass
 
-"""
-     Greedy search:
-         Pros:
-             - Can be faster than A* because it requires fewer calculations
-             - Simple to implement
-         Cons:
-             - Can get stuck in local minima and find suboptimal solutions
-             - May not always find a solution
+    #  Greedy search:
+    #      Pros:
+    #          - Can be faster than A* because it requires fewer calculations
+    #          - Simple to implement
+    #      Cons:
+    #          - Can get stuck in local minima and find suboptimal solutions
+    #          - May not always find a solution
      
-     Uniform cost search:
-         Pros:
-             - Guaranteed to find the optimal solution if one exists
-             - Simple to implement
-         Cons:
-             - Can be slower than A* because it expands all nodes, even if they are not promising
-             - May require a lot of memory to store all nodes
+    #  Uniform cost search:
+    #      Pros:
+    #          - Guaranteed to find the optimal solution if one exists
+    #          - Simple to implement
+    #      Cons:
+    #          - Can be slower than A* because it expands all nodes, even if they are not promising
+    #          - May require a lot of memory to store all nodes
      
-     A* algorithm:                                                                                           
-         Pros:                                                                                                
-             - Guaranteed to find the optimal solution if one exists                                          
-             - Can be more efficient than greedy search or uniform cost search because it uses a heuristic    
-               function to guide the search                                                                     
-         Cons:                                                                                                
-             - More computationally expensive than greedy search or uniform cost search because               
-               it requires more calculations                                                                  
-                                                                                                              
-"""
+    #  A* algorithm:                                                                                           
+    #      Pros:                                                                                                
+    #          - Guaranteed to find the optimal solution if one exists                                          
+    #          - Can be more efficient than greedy search or uniform cost search because it uses a heuristic    
+    #            function to guide the search                                                                     
+    #      Cons:                                                                                                
+    #          - More computationally expensive than greedy search or uniform cost search because               
+    #            it requires more calculations                                                                  
 
     def expand_state(self):
         # return every state possible for the current state
